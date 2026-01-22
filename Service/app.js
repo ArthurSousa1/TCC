560be1911d417f072b197fb9bc501f7ce650a063
Arthur Sousa <sousaarthur840@gmail.com>
2026-01-22 17:43:12 -0300

    Adding local requests to retrive questions and send answers

---
+++ Service/app.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const express = require('express');
+const { send } = require('process');
+
+const app = express();
+
+const questions = JSON.parse(fs.readFileSync(`${__dirname}/data/questions.json`, 'utf-8'));
+
+const getAllQuestion = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: questions.length,
+        data: questions
+    });
+}
+
+const getRandomQuestion = (req, res) => {
+    const questionsArray = questions.questions;
+    const randomIndex = Math.floor(Math.random() * questionsArray.length);
+    const randomQuestion = questionsArray[randomIndex];
+
+    res.status(200).json({
+        status: 'success',
+        data: randomQuestion
+    });
+}
+
+const sendAnswer = (req, res) => {
+    res.status(200).send('Calculando nota para a questão ' + req.params.questionId);
+}
+
+app.get('/api/v1/allQuestions', getAllQuestion);
+app.get('/api/v1/randomQuestion', getRandomQuestion);
+app.post('/api/v1/answer/:questionId', sendAnswer);
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
